7f6bc157956b7d0ebc8af868b09a7db0854ca1fe
sanyog <Sanyog Bhardwaj from Valiant>
2025-08-13 10:30:39 +0530

    4.0

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Published Version: 3.0
+          Published Version: 4.0
         </p>
         <a
           className="App-link"
